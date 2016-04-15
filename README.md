6546ad889ec0a2688402f280a55a442b7b2df839
James Green <james.mk.green@gmail.com>
2016-04-15 16:33:24 +0100

    Initial work. Now needs hooking up to a real InfluxDB target an exercising.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # spring-boot-actuator-influxdb
 A Spring MetricsWriter implementation for InfluxDB
+
+Basically a clone of the OpenTSDB writer then bent toward InfluxDB compatibility.
+
+Note that with InfluxDB there are both tags and fields that are conceptually similar
+and yet extensions to standard metrics.
+
+InfluxDB requires at least one field, so this is written as `value` with the value of the `Metric` value.
+
+For tags we currently re-use the OpenTSDB naming strategy.
---
+++ src/test/java/org/springframework/boot/actuate/metrics/influxdb/InfluxDBDataTest.java
@@ -0,0 +1,23 @@
+package org.springframework.boot.actuate.metrics.influxdb;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
+
+import java.util.Date;
+
+/**
+ * Created by jgreen on 4/15/2016.
+ */
+public class InfluxDBDataTest {
+
+    @Test
+    public void testToString() {
+        InfluxDBData data = new InfluxDBData();
+        data.setMetric("com.example.some-app.some-service.some-indicator");
+        data.setValue(1);
+        data.setTimestamp(new Date().getTime());
+        Assert.assertThat(data.toString(), JUnitMatchers.containsString("com.example.some-app.some-service.some-indicator "));
+        Assert.assertThat(data.toString(), JUnitMatchers.containsString("1i "));
+    }
+}
